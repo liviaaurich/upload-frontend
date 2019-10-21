7771e662a38f60eb25e7d71b8965f7d5760fde83
Livia Tononi Aurich <livia.aurich@basis.com.br>
2019-10-21 12:02:21 -0300

    Front-end quase terminado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,116 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
+
+import api from './services/api';
+
+import GlobalStyle from './styles/global';
+import { Container, Content } from './styles';
+
+import Upload from './components/Upload'
+import FileList from './components/FileList'
 
 class App extends Component {
+  state = {
+    uploadedFiles: [], //Armazena as informações dos arquivos que o usuário fez upload
+  }
+  
+  async componenteDidMount() {
+    const response = await api.get('posts');
+
+    this.setState({
+      uploadedFiles: response.data.map(file => ({
+        id: file.id,
+        name: file.name,
+        readableSize: filesize(file.size),
+        preview: file.url,
+        uploaded: true,
+        url: file.url
+      }))
+    })
+  }
+
+  handleUpload = files => {
+    const uploadedFiles = files.map(file => ({
+      file, 
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview:URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles) //Assim ele não sobrepõe o estado e sim adiciona mais informações
+    })
+
+    uploadedFiles.forEach(this.processUpload);
+  };
+
+  updateFile = (id, data) => {
+    this.setState({ 
+      uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+        return id === uploadedFile.id ? { ...uploadedFile, ...data } : uploadedFile;
+      }) 
+    });
+  };
+
+  processUpload = (uploadedFile) => {
+    const data = new FormData(); //Objeto que o html transforma nosso campos do formulário dentro do JS
+    
+    data.append('file', uploadedFile.file, uploadedFile.name);
+
+    api.post('/posts', data, {
+      onUploadProgress: e => {
+        const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+
+        this.updateFile(uploadedFile.id, {
+          progress
+        })
+      }
+    }).then(response => {
+      this.updateFile(uploadedFile.id, {
+        uploaded: true,
+        id: response.data._id,
+        url: response.data.url
+      });
+    }).catch(() => {
+      this.updateFile(uploadedFile.id, {
+        error: true
+      });
+    });
+  }
+
+  handleDelete = async id => {
+    await api.delete(`posts/${id}`);
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.filter(file => file.id !== id)
+    });
+  }
+
+  componentWillUnmount() {
+    //Deleta todos os objectsUrl que a gente criou anteriormente para não sobra mais nenhum cash de imagem
+    this.state.uploadedFiles.forEach(file => URL.revokeObjectURL(file.preview));
+  }
+
   render() {
-    return <div className="App" />
+    const { uploadedFiles } = this.state;
+
+    return (
+      <Container>
+        <Content>
+          <Upload onUpload={this.handleUpload} />
+          { !!uploadedFiles.length && (
+            <FileList files={uploadedFiles} onDelete={this.handleDelete}/>
+          ) }
+        </Content>
+        <GlobalStyle />
+      </Container>
+    );
   }
 }
 
--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
 
+import Dropzone from 'react-dropzone';
+
+import { DropContainer, UploadMessage } from './styles';
+
 // import { Container } from './styles';
 
 export default class Upload extends Component {
+  
+  renderDragMessage = (isDragActive, isDragReject) => {
+    if (!isDragActive) {
+      return <UploadMessage>Arraste arquivos aqui ...</UploadMessage>;
+    }
+
+    if (isDragReject) {
+      return <UploadMessage type="error">Arquivo não suportado</UploadMessage>;
+    }
+    
+    return <UploadMessage type="success">Solte os arquivos aqui</UploadMessage>;
+  };
+
   render() {
-    return <div />;
+    const { onUpload } = this.props;
+    
+    return (
+      <Dropzone accept="image/*" onDropAccepted={onUpload}>
+        { ({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer 
+            {...getRootProps()}
+            isDragActive={isDragActive} //Simboliza quando o usuário está com o mouse passando por cima dessa zone
+            isDragReject={isDragReject} //Quando o usuario esta passando um arquivo que nao é imagem
+          >
+            <input {...getInputProps()} />
+            {this.renderDragMessage(isDragActive, isDragReject)}
+          </DropContainer>
+        ) }
+      </Dropzone>  
+    );
   }
 }
